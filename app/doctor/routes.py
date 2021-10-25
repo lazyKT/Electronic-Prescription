f5158c5a9e5c4bd17fed77a8150c371bb97c210b
lazyKT <kyaw.thitlwin.me@gmail.com>
2021-10-26 00:17:16 +0800

    create prescription layout defined

--- app/doctor/routes.py
+++ app/doctor/routes.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask_login import login_required
 
 
-@bp('/create-rescription', methods=['GET', 'POST'])
+@bp.route ('/create-rescription', methods=['GET', 'POST'])
+@login_required
 def create_prescriptions():
     return render_template('doctor/create_prescriptions.html')
